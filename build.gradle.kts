3aada743976b48396890842775cc3d20e2bce90f
bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2022-07-10 22:57:49 +0200

    things and server shutdown message

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,42 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "fr.bananasmoothii"
 version = "1.0-SNAPSHOT"
 
+sourceSets {
+    main {
+        java {
+            srcDir("MCWFC/src/main/java")
+        }
+    }
+    test {
+        java {
+            srcDir("MCWFC/src/test/java")
+        }
+    }
+}
+
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.github.Minestom:Minestom:master-SNAPSHOT")
+    implementation("org.tinylog:slf4j-tinylog:2.4.1")
+    compileOnly("org.jetbrains:annotations:23.0.0")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks {
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to "fr.bananasmoothii.minestomtest.Main",
+                "Muti-Release" to "true",
+            )
+        }
+    }
 }
--- src/main/java/fr/bananasmoothii/minestomtest/Main.java
+++ src/main/java/fr/bananasmoothii/minestomtest/Main.java
@@ -1,13 +1,69 @@
-package fr.bananasmoothii;
+package fr.bananasmoothii.minestomtest;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.generator.UnitModifier;
+import org.tinylog.Logger;
+
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         // Initialize the server
         MinecraftServer minecraftServer = MinecraftServer.init();
+        MojangAuth.init();
+        Logger.info("Server version: " + MinecraftServer.getBrandName() + " " + MinecraftServer.VERSION_NAME);
 
-        // REGISTER EVENTS (set spawn instance, teleport player at spawn)
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator(unit -> {
+            final UnitModifier modifier = unit.modifier();
+            modifier.fillHeight(-64, -63, Block.BEDROCK);
+            modifier.fillHeight(-63, 60, Block.STONE);
+            modifier.fillHeight(60, 63, Block.DIRT);
+            modifier.fillHeight(63, 64, Block.GRASS_BLOCK);
+        });
 
-        // Start the server
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 64, 0));
+            player.setGameMode(GameMode.CREATIVE);
+        });
+
+        // Server shutdown custom message
+        MinecraftServer.getSchedulerManager().buildShutdownTask(() -> {
+            for (Instance instance : instanceManager.getInstances()) {
+                for (Player player : instance.getPlayers()) {
+                    player.kick(Component.text("Server is shutting down", TextColor.color(255, 0, 0)));
+                }
+            }
+        });
+
+        ForkJoinPool.commonPool().submit(() -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ignored) {
+            }
+            MinecraftServer.stopCleanly();
+        });
+
+        // Start the server on port 25565
         minecraftServer.start("0.0.0.0", 25565);
     }
 }
